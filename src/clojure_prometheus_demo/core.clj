5231d30261fa863cbcd26d0baf9f8b53c51b219b
Jo Draeger <jo.draeger@signal.uk.com>
2018-09-16 10:09:23 +0100

    generated from app template

---
+++ src/clojure_prometheus_demo/core.clj
@@ -0,0 +1,7 @@
+(ns clojure-prometheus-demo.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/clojure_prometheus_demo/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-prometheus-demo.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-prometheus-demo.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
